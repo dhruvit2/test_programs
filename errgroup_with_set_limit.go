cd903617dcc980f55d482c19e585f94543765ad2
dhruvit2 <32679878+dhruvit2@users.noreply.github.com>
2022-06-13 15:10:57 +0530

    Create errgroup_with_set_limit.go
    new creation will be block until the limit is freed

---
+++ errgroup_with_set_limit.go
@@ -0,0 +1,38 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+	var eg errgroup.Group
+	eg.SetLimit(2)
+	eg.Go(func() error {
+		for {
+			fmt.Printf("From go 1\n")
+			time.Sleep(1 * time.Second)
+		}
+	})
+
+	eg.Go(func() error {
+		for i := 0; i < 10; i++ {
+			time.Sleep(1 * time.Second)
+			fmt.Printf("From go 2\n")
+		}
+		return nil
+	})
+
+	eg.Go(func() error {
+		for {
+			fmt.Printf("From go 3\n")
+		}
+	})
+
+	_ = eg.Wait()
+}
